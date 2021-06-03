27528dcf02e2283cfa327ef864db3f7dde57c182
omar-compres <omaromch@gmail.com>
2021-06-03 19:35:58 -0400

    Adding scenarios for Valet Parking

--- pom/constants/contants.py
+++ pom/constants/contants.py
@@ -5,5 +5,8 @@ urls = {
 expected_values = {
     "negative_difference": "ERROR! YOUR LEAVING DATE OR TIME IS BEFORE YOUR STARTING DATE OR TIME",
     "incorrect_date_value": "ERROR! ENTER A CORRECTLY FORMATTED DATE",
-    "incorrect_time_value": "ERROR! UNEXPECTED TIME VALUE. PLEASE MAKE SURE TO USE THIS FORMAT: {HH:MM}"
+    "incorrect_time_value": "ERROR! UNEXPECTED TIME VALUE. PLEASE MAKE SURE TO USE THIS FORMAT: {HH:MM}",
+    "valet_less_than_5_hours": "$ 12.00",
+    "valet_over_5_less_than_24": "$ 18.00",
+    "valet_7_days_price": "$ 126.00"
 }
--- tests/test_valet_parking.py
+++ tests/test_valet_parking.py
@@ -1,8 +1,69 @@
 from pom.factory import Factory
 from pom.constants.contants import expected_values
-import time
-
-from pom.utils import Utils
 
 
-# class TestValetParking:
+class TestValetParking:
+
+    def test_few_minutes_difference(self):
+        page = Factory.parking_cost_calculator
+        page.get()
+        page.starting_date_input.send_keys("6/30/2021")
+        page.starting_time_am_radio.click()
+        page.starting_time_input.send_keys("06:00")
+        page.leaving_date_input.send_keys("6/30/2021")
+        page.leaving_time_am_radio.click()
+        page.leaving_time_input.send_keys("06:02")
+        page.calculate_button.click()
+        assert Factory.utils.is_text_equal(expected_values['valet_less_than_5_hours'], page.price_feedback_message.text)
+
+    def test_few_hours_less_than_5(self):
+        page = Factory.parking_cost_calculator
+        page.get()
+        page.starting_date_input.send_keys("6/30/2021")
+        page.starting_time_am_radio.click()
+        page.starting_time_input.send_keys("06:00")
+        page.leaving_date_input.send_keys("6/30/2021")
+        page.leaving_time_am_radio.click()
+        page.leaving_time_input.send_keys("10:59")
+        page.calculate_button.click()
+        assert Factory.utils.is_text_equal(expected_values['valet_less_than_5_hours'], page.price_feedback_message.text)
+
+    def test_for_slightly_over_5_hours(self):
+        page = Factory.parking_cost_calculator
+        page.get()
+        page.starting_date_input.send_keys("6/30/2021")
+        page.starting_time_am_radio.click()
+        page.starting_time_input.send_keys("06:00")
+        page.leaving_date_input.send_keys("6/30/2021")
+        page.leaving_time_am_radio.click()
+        page.leaving_time_input.send_keys("11:15")
+        page.calculate_button.click()
+        assert Factory.utils.is_text_equal(expected_values['valet_over_5_less_than_24'],
+                                           page.price_feedback_message.text)
+
+    def test_for_slightly_under_24_hours(self):
+        page = Factory.parking_cost_calculator
+        page.get()
+        page.starting_date_input.send_keys("6/29/2021")
+        page.starting_time_am_radio.click()
+        page.starting_time_input.send_keys("06:00")
+        page.leaving_date_input.send_keys("6/30/2021")
+        page.leaving_time_am_radio.click()
+        page.leaving_time_input.send_keys("05:59")
+        page.calculate_button.click()
+        assert Factory.utils.is_text_equal(expected_values['valet_over_5_less_than_24'],
+                                           page.price_feedback_message.text)
+
+    def test_for_a_couple_of_days(self):
+        page = Factory.parking_cost_calculator
+        page.get()
+        page.starting_date_input.send_keys("6/29/2021")
+        page.starting_time_am_radio.click()
+        page.starting_time_input.send_keys("06:00")
+        page.leaving_date_input.send_keys("7/06/2021")
+        page.leaving_time_am_radio.click()
+        page.leaving_time_input.send_keys("06:00")
+        page.calculate_button.click()
+        assert Factory.utils.is_text_equal(expected_values['valet_7_days_price'],
+                                           page.price_feedback_message.text)
+
