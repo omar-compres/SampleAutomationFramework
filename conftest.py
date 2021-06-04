e3c2a5665bdf63e0edb0eb03da9a527766a6d476
omar-compres <omaromch@gmail.com>
2021-06-04 12:05:54 -0400

    Setting up the capturing of screenshots and logs at teardown

--- conftest.py
+++ conftest.py
@@ -1,9 +1,20 @@
 from pom.driver.driver import Driver
+import pytest
+import os
+import allure
 
 
 def pytest_runtest_setup():
     Driver.initialize()
 
 
-def pytest_runtest_teardown():
+def pytest_runtest_teardown(item):
+    filename = 'screenshot-' + item.name
+    log = Driver.driver.get_log('browser')
+
+    Driver.driver.get_screenshot_as_file(os.path.abspath(os.path.join('.', 'screenshots', filename)))
+
+    allure.attach(Driver.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=allure.attachment_type.PNG)
+    allure.attach(str(log), name="Console Logs", attachment_type=allure.attachment_type.TEXT)
+
     Driver.quit()
